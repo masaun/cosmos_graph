36f63995b6adf9840a9e304d778b3687f19dfd70
Jon Huang <biohfj@gmail.com>
2018-11-04 07:41:03 +0100

    update worker

--- worker.py
+++ worker.py
@@ -4,6 +4,7 @@ from logging import getLogger
 import sys
 import logging
 from Queue import Queue
+from py2neo import Graph
 
 log = getLogger()
 log.setLevel(logging.DEBUG)
@@ -14,14 +15,11 @@ log.addHandler(ch)
 seed_url = 'https://gaia-seeds.interblock.io/net_info'
 queried = []
 
-
 queue = Queue()
 
-from neo4jrestclient.client import GraphDatabase
-url = "http://neo4j:test1234@localhost:7474/db/data/"
-gdb = GraphDatabase(url)
-# All Delete
-gdb.query("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r", data_contents=True)
+graph = Graph(host='localhost', user='neo4j', password='test1234')
+graph.run("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
+
 
 def main():
     while True:
@@ -30,10 +28,10 @@ def main():
         try:
             # if queue is empty, we do query from origin
             if queue.empty():
-                queue.put((0,seed_url))
+                queue.put(('0', seed_url))
 
             # get one peer which we don't queried before
-            peer_id, query_url = queue.get()
+            centre_peer_id, query_url = queue.get()
             print query_url
             if query_url in queried:
                 continue
@@ -42,6 +40,7 @@ def main():
             result = res.json()
             peers = []
             for peer in result['result']['peers']:
+                peer_id = peer['node_info']['id']
                 # get the address
                 addr = peer['node_info']['listen_addr'].split(':', 1)[0]
 
@@ -50,7 +49,7 @@ def main():
                     rpc_addr = [element for element in peer['node_info']['other'] if 'rpc_addr' in element]
                     rpc_port = rpc_addr[0].rsplit(':', 1)[1] if rpc_addr else '26657'
                     rpc_url = 'http://{}:{}/net_info'.format(addr, rpc_port)
-                    peer_id = peer['node_info']['id']
+
                     queue.put((peer_id, rpc_url))
 
                 # store peers
@@ -61,15 +60,16 @@ def main():
                 queried.append(query_url)
 
             log.debug(peers)
-            centre_peer = gdb.nodes.create(id=peer_id)
+            graph.run('CREATE (peer:Peer {name:"' + centre_peer_id + '"}) RETURN peer')
             for peer_id, peer_ip in peers:
-                peer_node = gdb.nodes.create(id=peer_id, ip=peer_ip)
-                centre_peer.relationships.create("link", peer_node)
-
+                graph.run('CREATE (peer:Peer {name:"' + peer_id + '"}) RETURN peer')
+                graph.run(
+                    'MATCH (n{name:"' + centre_peer_id + '"}),(m{name:"' + peer_id + '"}) CREATE (n)<-[r:CONN]-(m)')
 
         except Exception, e:
-            log.debug(e.message)
+            log.exception(e.message)
             continue
+
 
 if __name__ == '__main__':
     main()
